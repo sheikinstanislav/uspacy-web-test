7f374d09315f292bdb8cc4a9a01a27666aca175c
Stanislav <43264494+sheikinstanislav@users.noreply.github.com>
2024-09-29 16:29:16 +0300

    chore(web): update validation messages

--- src/shared/helpers/validation.ts
+++ src/shared/helpers/validation.ts
@@ -5,30 +5,30 @@ const phoneNumberRegEx = /^\+[1-9]\d{1,14}$/;
 export const validationSchema = yup.object().shape({
   firstName: yup
     .string()
-    .min(3, 'Too Short !')
-    .max(30, 'Too Long !')
+    .min(3, 'Занадто коротке!')
+    .max(30, 'Занадто довге!')
     .required("Це поле обов'язкове"),
   lastName: yup
     .string()
-    .min(3, 'Too Short !')
-    .max(30, 'Too Long !')
+    .min(3, 'Занадто коротке!')
+    .max(30, 'Занадто довге!')
     .required("Це поле обов'язкове"),
 
   companyName: yup
     .string()
-    .min(3, 'Too Short !')
-    .max(30, 'Too Long !')
+    .min(3, 'Занадто коротке!')
+    .max(30, 'Занадто довге!')
     .required("Це поле обов'язкове"),
 
   email: yup
     .string()
-    .email('Enter a Vaid Email')
+    .email('Введіть дійсну електронну адресу')
     .required("Це поле обов'язкове"),
 
   phoneNumber: yup
     .string()
-    .matches(phoneNumberRegEx, 'Invalid Phone Number')
-    .min(5, 'Too Short !')
-    .max(15, 'Invalid Phone Number')
+    .matches(phoneNumberRegEx, 'Невірний номер телефону')
+    .min(5, 'Занадто короткий!')
+    .max(15, 'Невірний номер телефону')
     .required("Це поле обов'язкове"),
 });
